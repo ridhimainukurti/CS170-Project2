02f7a3af6969397af3dc1a6d4c409cd06b8fd6b2
JesusMontes229 <jmmontes217@gmail.com>
2024-11-20 11:20:54 -0800

    continuing adding testing rig

---
+++ test/CMakeFiles/runAllTests.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/csmajs/jmont229/cs170/CS170-Project2/test/test.cpp" "CMakeFiles/runAllTests.dir/test.cpp.o" "gcc" "CMakeFiles/runAllTests.dir/test.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ test/CMakeFiles/runAllTests.dir/test.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/runAllTests.dir/test.cpp.o: \
+ /home/csmajs/jmont229/cs170/CS170-Project2/test/test.cpp \
+ /usr/include/stdc-predef.h
