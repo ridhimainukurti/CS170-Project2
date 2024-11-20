191e15deb13ad744d4de765fd10579efdd9cf6de
JesusMontes229 <jmmontes217@gmail.com>
2024-11-20 15:31:29 -0800

    added feature node

--- header/Evaluation.h
+++ header/Evaluation.h
@@ -10,10 +10,14 @@ struct FeaturesAndAccuracy
     std::set<int> _features;
     double _accuracy;
 };
+struct featureNode
+{
+    std::set<int>_features;
+};
 
 //adjust functions in part 2 to do k fold validation and to deal with classifiers
-double GetAccurracy(std::set<int>); //adjust in part 2 to do k fold validation
+double GetAccurracy(featureNode); //adjust in part 2 to do k fold validation
 const std::vector<FeaturesAndAccuracy> Forward_Selection(const std::set<int>&, int numberOfFeatures);
-const std::vector<FeaturesAndAccuracy> Backwards_Elimination(const std::set<int>&, int numberOfFeatures);
+const std::vector<FeaturesAndAccuracy> Backwards_Elimination(const std::set<int>&, int numberOfFeatures, int size_of_set);
 
 #endif
